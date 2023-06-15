a38233755df8605f93bb34363cc1de549b12817b
eness2 <sevim.enes06@gmail.com>
2023-06-15 19:17:26 +0300

    login screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gurme/features/auth/screen/login_screen.dart';
 import 'package:gurme/firebase_options.dart';
 
 void main() async {
@@ -9,15 +10,22 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Gurme',
+      home: LoginScreen(),
+    );
   }
 }
