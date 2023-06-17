cd1be2627d742268b415a4da9dab5e47f2ef9e45
eness2 <sevim.enes06@gmail.com>
2023-06-17 23:28:36 +0300

    anonymous_user.png path added

--- lib/common/constants/asset_constants.dart
+++ lib/common/constants/asset_constants.dart
@@ -3,4 +3,5 @@ class AssetConstants {
 
   static String defaultProfilePic =
       'https://thenounproject.com/api/private/icons/82456/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0';
+  static String anonymousUser = 'assets/images/anonymous_user.png';
 }
