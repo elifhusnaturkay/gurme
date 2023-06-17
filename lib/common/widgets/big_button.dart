a568247936339683502ca3419750650b42bfa487
eness2 <sevim.enes06@gmail.com>
2023-06-17 23:29:48 +0300

    common button widgets added

---
+++ lib/common/widgets/big_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BigButton extends StatelessWidget {
+  const BigButton({super.key, required this.onTap, required this.text});
+
+  final Function()? onTap;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onTap;
+      },
+      child: Container(
+        height: 51,
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(
+          color: Colors.indigo.shade400,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: GoogleFonts.inter(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/common/widgets/square_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  const SquareTile({super.key, required this.imagePath, required this.onTap});
+
+  final String imagePath;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 0,
+              blurRadius: 4,
+              offset: const Offset(0, 4), // changes the position of the shadow
+            ),
+          ],
+        ),
+        child: Image.asset(
+          imagePath,
+          height: 85,
+        ),
+      ),
+    );
+  }
+}
