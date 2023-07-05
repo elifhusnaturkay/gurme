97a13fdbbfc26715725e6ae8ea857271ab818448
eness2 <sevim.enes06@gmail.com>
2023-07-05 22:45:29 +0300

    main updated for updating according to userdata

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gurme/common/widgets/loading_spinner.dart';
+import 'package:gurme/features/auth/controller/auth_controller.dart';
+import 'package:gurme/features/splash/controller/splash_controller.dart';
 import 'package:gurme/firebase_options.dart';
+import 'package:gurme/models/user_model.dart';
 import 'package:gurme/router.dart';
 
 final themeProvider = StateProvider<ThemeData>((ref) => ThemeData.light());
@@ -21,21 +26,53 @@ void main() async {
   );
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Gurme',
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      routeInformationParser: ref.watch(routerProvider).routeInformationParser,
-      routeInformationProvider:
-          ref.watch(routerProvider).routeInformationProvider,
-      routerDelegate: ref.watch(routerProvider).routerDelegate,
-      theme: ref.watch(themeProvider),
-    );
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  UserModel? userModel;
+  void getUserData(User user) async {
+    userModel = await ref
+        .watch(authControllerProvider.notifier)
+        .getUserData(user.uid)
+        .first;
+
+    ref.read(userProvider.notifier).update((state) => userModel);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ref.watch(authStateChangeProvider).when(
+          data: (user) {
+            if (user != null) {
+              getUserData(user);
+            }
+            if (userModel == null) {
+              ref
+                  .read(authControllerProvider.notifier)
+                  .signInAnonymously(context);
+            }
+            return MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              title: 'Gurme',
+              locale: DevicePreview.locale(context),
+              builder: DevicePreview.appBuilder,
+              routeInformationParser:
+                  ref.watch(routerProvider).routeInformationParser,
+              routeInformationProvider:
+                  ref.watch(routerProvider).routeInformationProvider,
+              routerDelegate: ref.watch(routerProvider).routerDelegate,
+              theme: ref.watch(themeProvider),
+            );
+          },
+          error: (error, stackTrace) {
+            return Text(error.toString());
+          },
+          loading: () => const LoadingSpinner(),
+        );
   }
 }
