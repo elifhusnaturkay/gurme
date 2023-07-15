e5fc5cfa4bcc8b12308466def85df0b88a763318
ahmetoozcan <ahmetozcan21@yahoo.com>
2023-07-15 03:00:28 +0300

    tabview updated for zero favorite company and zero comments & favorite drawer library import fix

--- lib/features/home/drawers/favorites_drawer.dart
+++ lib/features/home/drawers/favorites_drawer.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:gurme/common/constants/asset_constants.dart';
 import 'package:gurme/common/constants/route_constants.dart';
 import 'package:gurme/features/auth/controller/auth_controller.dart';
 import 'package:gurme/features/home/controller/home_controller.dart';
